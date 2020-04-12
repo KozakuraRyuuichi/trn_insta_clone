a56aa99c1db48b321bfa0704f9075e2514413744
KozakuraRyuuichi <k267280.352@gmail.com>
2020-04-12 16:00:33 +0900

    [update]usernameインデックスにもunique制約を追加
    rubocop -aで自動修正されなかったため、手動修正した（3/3）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_28_134937) do
+ActiveRecord::Schema.define(version: 2020_04_12_064253) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_03_28_134937) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
