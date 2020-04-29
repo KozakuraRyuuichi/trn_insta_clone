a3e9f8e11d9158419ee37e6941a2c837f5868bcf
KozakuraRyuuichi <k267280.352@gmail.com>
2020-04-30 00:06:48 +0900

    [add]ルーティングにpostsを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#new'
 
-  resources :users, only: %i[new create]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :users, only: %i[new create]
+  resources :posts
 end
