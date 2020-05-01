69d515c5ab60688fdb29415ed982972e0fb383e1
KozakuraRyuuichi <k267280.352@gmail.com>
2020-05-01 23:50:20 +0900

    [add]posts/indexページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
+  # Sorceryでデフォルトで実装されている
+  before_action :require_login, only: %i[new show edit]
   def index
+    # postと関連付けされているuserテーブルからデータを取得する
+    # 絞り込みを行うため、includesメソッドを選択する
+    @posts = Post.includes(:user).order(created_at: :desc)
   end
 
   def new
