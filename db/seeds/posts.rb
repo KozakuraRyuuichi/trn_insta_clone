6202f32d7bc38de217dda27dd9a2044b675ff54a
KozakuraRyuuichi <k267280.352@gmail.com>
2020-05-03 13:28:00 +0900

    [update]seedの作成状況が分かるように修正

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,8 +1,10 @@
+puts 'Start inserting seed "posts" ...'
 # 50を上限とする
 User.limit(50).each do |user|
   # userと関連付けさせるからこういう記述にする。tutorialの13章参照
   post = user.posts.create(
     content: Faker::Hacker.say_something_smart,
-    remote_images_urls: %w[https://picsum.photos/350/350/?random]
+    remote_images_urls: %w[https://picsum.photos/350/350/?random https://picsum.photos/350/350/?random]
   )
+  puts "post#{post.id} has created!"
 end
--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,8 +1,10 @@
+puts 'Start inserting seed "users" ...'
 100.times do |n|
-  User.create(
+  user = User.create(
     email: Faker::Internet.unique.email,
     username: Faker::Name.unique.name,
     password: 'password',
     password_confirmation: 'password'
   )
+  puts "\"#{user.username}\" has created!"
 end
