d9ce629861248226a270a3897b3eba8a13d95b2b
KozakuraRyuuichi <k267280.352@gmail.com>
2020-05-04 15:44:50 +0900

    [update]i18nのpath設定を追加

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module TrnInstaClone
     config.generators.system_tests = nil
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # TimeZoneの設定
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
